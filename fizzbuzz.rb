56b5abacb78f8c8cdecf5e60f8294b2a8e010f25
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-02-16 00:13:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,10 @@
 def fizzbuzz(int)
   if int % 3 == 0
-    puts "Fizz"
+    "Fizz"
 elsif int % 5 == 0
-  puts "Buzz"
+  "Buzz"
 else int % 3 && 5 == 0
-    puts "FizzBuzz"
+  "FizzBuzz"
   end
 end
+
